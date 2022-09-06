9e6d01159c341614680c18f4d50053ed153035f6
Chris Maki <c.william.developer@gmail.com>
2022-09-06 16:46:04 -0500

    #8 stretch goal complete

--- assignment/scripts/function-practice.js
+++ assignment/scripts/function-practice.js
@@ -83,12 +83,12 @@ function find(value, array) {
       }
       if(found) {
         return true;
-      } else {
+       } else {
         return false;
       }
  
   };
-  console.log(find(88,[55,4,88,2]));
+  console.log(find(88,[55,4,88,2]) );
 
 
 // ----------------------
@@ -97,11 +97,15 @@ function find(value, array) {
 // 8. Function to check if a letter is the first letter in a 
 //    string. Return true if it is, and false otherwise
 function isFirstLetter(letter, string) {
-
+if(letter == string[0]) {
+  return true;
+} else {
+  return false;
 }
+};
+
 console.log( 'isFirstLetter - should say true', isFirstLetter('a', 'apple') );
 console.log( 'isFirstLetter - should say false', isFirstLetter('z', 'apple') );
-
 // 9. Function to return the sum of all numbers in an array
 function sumAll( ) {
   let sum = 0
